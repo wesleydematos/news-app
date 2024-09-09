d144e01ca407a48b150c29139b5f76485f9e1be7
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-09-09 09:09:30 -0300

    feat: add Head and Searchbar to home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Header } from "@/components/Header";
+import SearchBar from "@/components/SearchBar";
 
 type Props = {};
 
 const Page = (props: Props) => {
+  const { top: safeTop } = useSafeAreaInsets();
+
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
+    <View style={(styles.container, { paddingTop: safeTop })}>
+      <Header />
+      <SearchBar />
     </View>
   );
 };
@@ -16,7 +22,5 @@ export default Page;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
