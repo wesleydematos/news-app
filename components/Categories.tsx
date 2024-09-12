17acb0a5483c7fa476e2046e523acdda2d7959fa
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-09-12 15:05:34 -0300

    feat: create category buttons

---
+++ components/Categories.tsx
@@ -0,0 +1,92 @@
+import newsCategoryList from "@/constants/Categories";
+import { Colors } from "@/constants/Colors";
+import { useRef, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+
+type Props = {
+  onCategoryChanged: (category: string) => void;
+};
+
+export default function Categories({ onCategoryChanged }: Props) {
+  const scrollRef = useRef<ScrollView>(null);
+  const itemRef = useRef<TouchableOpacity[] | null[]>([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSelectCategory = (index: number) => {
+    const selected = itemRef.current[index];
+    setActiveIndex(index);
+
+    selected?.measure((x) => {
+      scrollRef.current?.scrollTo({ x: x - 20, y: 0, animated: true });
+    });
+
+    onCategoryChanged(newsCategoryList[index].slug);
+  };
+
+  return (
+    <View>
+      <Text style={styles.title}>Trending Right Now</Text>
+      <ScrollView
+        ref={scrollRef}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.itemsWrapper}
+      >
+        {newsCategoryList.map((item, index) => (
+          <TouchableOpacity
+            ref={(el) => (itemRef.current[index] = el)}
+            key={index}
+            style={[styles.item, activeIndex === index && styles.itemActive]}
+            onPress={() => handleSelectCategory(index)}
+          >
+            <Text style={[, activeIndex === index && styles.itemTxtActive]}>
+              {item.title}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: Colors.black,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  itemsWrapper: {
+    gap: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 10,
+  },
+  item: {
+    borderWidth: 1,
+    borderColor: Colors.darkGrey,
+    paddingVertical: 10,
+    paddingHorizontal: 61,
+    borderRadius: 10,
+  },
+  itemActive: {
+    backgroundColor: Colors.tint,
+    borderColor: Colors.tint,
+  },
+  itemTxt: {
+    fontSize: 14,
+    color: Colors.darkGrey,
+    letterSpacing: 0.5,
+  },
+  itemTxtActive: {
+    fontWeight: "600",
+    color: Colors.white,
+  },
+});
