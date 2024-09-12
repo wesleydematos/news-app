b707eef07d87b41c6dc038fba446b9a09317951d
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-09-12 14:37:35 -0300

    feat: scroll breaking news card

--- components/BreakingNews.tsx
+++ components/BreakingNews.tsx
@@ -1,13 +1,22 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ViewToken,
+  useWindowDimensions,
+} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
 import { Colors } from "@/constants/Colors";
 import { NewsDataType } from "@/types";
 import SliderItem from "./SliderItem";
 import Animated, {
+  scrollTo,
   useAnimatedRef,
   useAnimatedScrollHandler,
+  useDerivedValue,
   useSharedValue,
 } from "react-native-reanimated";
+import Pagination from "./Pagination";
 
 type Props = {
   newsList: Array<NewsDataType>;
@@ -15,15 +24,58 @@ type Props = {
 
 export default function BreakingNews({ newsList }: Props) {
   const [data, setData] = useState(newsList);
-  // const [paginationIndex, setPaginationIndex] = useState(0);
+  const [paginationIndex, setPaginationIndex] = useState(0);
   const scrollX = useSharedValue(0);
   const ref = useAnimatedRef<Animated.FlatList<any>>();
+  const [isAutoPlay, setIsAutoPlay] = useState(true);
+  const interval = useRef<NodeJS.Timeout>();
+  const offset = useSharedValue(0);
+  const { width } = useWindowDimensions();
 
   const onScrollHandler = useAnimatedScrollHandler({
     onScroll: (e) => {
       scrollX.value = e.contentOffset.x;
     },
+    onMomentumEnd: (e) => {
+      offset.value = e.contentOffset.x;
+    },
   });
+
+  useEffect(() => {
+    console.log(width);
+    if (isAutoPlay === true) {
+      interval.current = setInterval(() => {
+        offset.value = offset.value + width;
+      }, 5000);
+    } else {
+      clearInterval(interval.current);
+    }
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, [isAutoPlay, offset, width]);
+
+  useDerivedValue(() => {
+    scrollTo(ref, offset.value, 0, true);
+  });
+
+  const onViewableItemsChanged = ({
+    viewableItems,
+  }: {
+    viewableItems: ViewToken[];
+  }) => {
+    if (viewableItems[0].index != undefined && viewableItems[0].index != null) {
+      setPaginationIndex(viewableItems[0].index % newsList.length);
+    }
+  };
+
+  const viewabilityConfig = {
+    itemVisiblePercentThreshold: 50,
+  };
+
+  const viewabilityConfigCallbackPairs = useRef([
+    { viewabilityConfig, onViewableItemsChanged },
+  ]);
 
   return (
     <View style={styles.container}>
@@ -42,9 +94,19 @@ export default function BreakingNews({ newsList }: Props) {
           pagingEnabled
           onScroll={onScrollHandler}
           scrollEventThrottle={16}
-          // onEndReachedThreshold={0.5}
-          // onEndReached={() => setData({ ...data, ...newsList })}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => setData([...data, ...newsList])}
+          viewabilityConfigCallbackPairs={
+            viewabilityConfigCallbackPairs.current
+          }
+          onScrollBeginDrag={() => {
+            setIsAutoPlay(false);
+          }}
+          onScrollEndDrag={() => {
+            setIsAutoPlay(true);
+          }}
         />
+        <Pagination items={newsList} paginationIndex={paginationIndex} />
       </View>
     </View>
   );
