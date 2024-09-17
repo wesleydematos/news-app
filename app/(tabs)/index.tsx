6b378045a667e27eba464c71835e9d129aef9130
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-09-17 09:19:31 -0300

    feat: create NewsList

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Header } from "@/components/Header";
@@ -7,16 +7,20 @@ import axios from "axios";
 import { NewsDataType } from "@/types";
 import BreakingNews from "@/components/BreakingNews";
 import Categories from "@/components/Categories";
+import NewsList from "@/components/NewsList";
+import Loading from "@/components/Loading";
 
 type Props = {};
 
 const Page = (props: Props) => {
   const { top: safeTop } = useSafeAreaInsets();
   const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([]);
+  const [news, setNews] = useState<NewsDataType[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getBreakingNews();
+    getNews();
   }, []);
 
   const getBreakingNews = async () => {
@@ -34,21 +38,43 @@ const Page = (props: Props) => {
     }
   };
 
+  const getNews = async (category: string = "") => {
+    try {
+      let categoryString = "";
+      if (category.length !== 0) {
+        categoryString = `&category=${category}`;
+      }
+
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&removeduplicate=1&size=10${categoryString}`;
+
+      const response = await axios.get(URL);
+
+      if (response && response.data) {
+        setNews(response.data.results);
+        setIsLoading(false);
+      }
+    } catch (error: any) {
+      console.log("Error message:", error.messsage);
+    }
+  };
+
   const onCatChanged = (category: string) => {
-    console.log("Category:", category);
+    setNews([]);
+    getNews(category);
   };
 
   return (
-    <View style={(styles.container, { paddingTop: safeTop })}>
+    <ScrollView style={(styles.container, { paddingTop: safeTop })}>
       <Header />
       <SearchBar />
       {isLoading ? (
-        <ActivityIndicator size="large" />
+        <Loading size="large" />
       ) : (
         <BreakingNews newsList={breakingNews} />
       )}
       <Categories onCategoryChanged={onCatChanged} />
-    </View>
+      <NewsList newsList={news} />
+    </ScrollView>
   );
 };
 
