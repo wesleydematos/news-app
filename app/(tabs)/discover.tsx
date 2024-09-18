b9d6538c8f3fbae6204a17f35181a812aa5fced1
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-09-18 10:04:32 -0300

    feat: update discover page

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -1,13 +1,60 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
+import SearchBar from "@/components/SearchBar";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Colors } from "@/constants/Colors";
+import CheckBox from "@/components/CheckBox";
+import { useNewsCategories } from "@/hooks/useNewsCategories";
+import { useNewsCountries } from "@/hooks/useNewsCountries";
 
 type Props = {};
 
 const Page = (props: Props) => {
+  const { top: safeTop } = useSafeAreaInsets();
+  const { newsCategories, toggleNewsCategory } = useNewsCategories();
+  const { newsCountries, toggleNewsCountries } = useNewsCountries();
+
   return (
-    <View style={styles.container}>
-      <Text>Discover Screen</Text>
-    </View>
+    <ScrollView style={[styles.container, { paddingTop: safeTop + 20 }]}>
+      <SearchBar withHorizontalPadding={false} />
+      <Text style={styles.title}>Categories</Text>
+      <View style={styles.listContainer}>
+        {newsCategories.map((item) => (
+          <CheckBox
+            key={item.id}
+            label={item.title}
+            checked={item.selected}
+            onPress={() => {
+              toggleNewsCategory(item.id);
+            }}
+          />
+        ))}
+      </View>
+
+      <Text style={styles.title}>Country</Text>
+      <View style={styles.listContainer}>
+        {newsCountries.map((item) => (
+          <CheckBox
+            key={item.id}
+            label={item.name}
+            checked={item.selected}
+            onPress={() => {
+              toggleNewsCountries(item.id);
+            }}
+          />
+        ))}
+      </View>
+
+      <TouchableOpacity style={styles.searchBtn}>
+        <Text style={styles.searchBtnTxt}>Search</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
@@ -16,7 +63,32 @@ export default Page;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    marginTop: 20,
+    fontSize: 18,
+    fontWeight: "600",
+    color: Colors.black,
+    marginBottom: 10,
+  },
+  listContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: 16,
+    marginTop: 12,
+    marginBottom: 20,
+  },
+  searchBtn: {
+    backgroundColor: Colors.tint,
     alignItems: "center",
+    padding: 14,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  searchBtnTxt: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
