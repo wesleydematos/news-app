a3ebd0b5a0c33342af738bfc5d6c4f18fb890c96
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-09-18 10:04:20 -0300

    feat: create CheckBox

---
+++ components/CheckBox.tsx
@@ -0,0 +1,80 @@
+import { Colors } from "@/constants/Colors";
+import { AntDesign } from "@expo/vector-icons";
+import { StyleSheet } from "react-native";
+import Animated, {
+  FadeIn,
+  FadeOut,
+  LinearTransition,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+
+type Props = {
+  label: string;
+  checked: boolean;
+  onPress: () => void;
+};
+
+export default function CheckBox({ checked, label, onPress }: Props) {
+  const rnAnimatedContainerStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: withTiming(
+        checked ? "rgba(239, 142, 82, 0.1)" : "transparent",
+        { duration: 150 }
+      ),
+      borderColor: withTiming(checked ? Colors.tint : Colors.black, {
+        duration: 150,
+      }),
+      paddingLeft: 16,
+      paddingRight: checked ? 10 : 16,
+    };
+  }, [checked]);
+
+  const rnTextStyle = useAnimatedStyle(() => {
+    return {
+      color: withTiming(checked ? Colors.tint : Colors.black, {
+        duration: 150,
+      }),
+    };
+  }, [checked]);
+
+  return (
+    <Animated.View
+      style={[styles.container, rnAnimatedContainerStyle]}
+      onTouchEnd={onPress}
+      layout={LinearTransition.springify().mass(0.8)}
+    >
+      <Animated.Text style={[styles.label, rnTextStyle]}>{label}</Animated.Text>
+      {checked && (
+        <Animated.View
+          style={styles.iconWrapper}
+          entering={FadeIn.duration(350)}
+          exiting={FadeOut}
+        >
+          <AntDesign name="checkcircle" size={14} color={Colors.tint} />
+        </Animated.View>
+      )}
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: Colors.black,
+    borderWidth: 1,
+    paddingVertical: 8,
+    borderRadius: 32,
+  },
+  label: {
+    fontSize: 14,
+    color: Colors.black,
+  },
+  iconWrapper: {
+    marginLeft: 8,
+    height: 14,
+    width: 14,
+  },
+});
