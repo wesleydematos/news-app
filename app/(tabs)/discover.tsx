12c8d28fc9008d0b0d7422e03dba91d646122ff1
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-09-19 09:47:48 -0300

    feat: update discover page

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -5,13 +5,14 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "@/components/SearchBar";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Colors } from "@/constants/Colors";
 import CheckBox from "@/components/CheckBox";
 import { useNewsCategories } from "@/hooks/useNewsCategories";
 import { useNewsCountries } from "@/hooks/useNewsCountries";
+import { Link } from "expo-router";
 
 type Props = {};
 
@@ -19,10 +20,16 @@ const Page = (props: Props) => {
   const { top: safeTop } = useSafeAreaInsets();
   const { newsCategories, toggleNewsCategory } = useNewsCategories();
   const { newsCountries, toggleNewsCountries } = useNewsCountries();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [category, setCategory] = useState("");
+  const [country, setCountry] = useState("");
 
   return (
     <ScrollView style={[styles.container, { paddingTop: safeTop + 20 }]}>
-      <SearchBar withHorizontalPadding={false} />
+      <SearchBar
+        withHorizontalPadding={false}
+        setSearchQuery={setSearchQuery}
+      />
       <Text style={styles.title}>Categories</Text>
       <View style={styles.listContainer}>
         {newsCategories.map((item) => (
@@ -32,6 +39,7 @@ const Page = (props: Props) => {
             checked={item.selected}
             onPress={() => {
               toggleNewsCategory(item.id);
+              setCategory(item.slug);
             }}
           />
         ))}
@@ -46,14 +54,23 @@ const Page = (props: Props) => {
             checked={item.selected}
             onPress={() => {
               toggleNewsCountries(item.id);
+              setCountry(item.code);
             }}
           />
         ))}
       </View>
 
-      <TouchableOpacity style={styles.searchBtn}>
-        <Text style={styles.searchBtnTxt}>Search</Text>
-      </TouchableOpacity>
+      <Link
+        href={{
+          pathname: `/news/search`,
+          params: { query: searchQuery, category, country },
+        }}
+        asChild
+      >
+        <TouchableOpacity style={styles.searchBtn}>
+          <Text style={styles.searchBtnTxt}>Search</Text>
+        </TouchableOpacity>
+      </Link>
     </ScrollView>
   );
 };
